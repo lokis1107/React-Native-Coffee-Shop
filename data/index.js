d10b84cc73fdeed9e5b6463ab1b6c769e69e7c60
Logesh S <139110018+lokis1107@users.noreply.github.com>
2023-07-31 17:38:01 +0530

    Add files via upload

---
+++ data/index.js
@@ -0,0 +1,94 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Cappuccino",
+  },
+  {
+    id: 2,
+    title: "Latte",
+  },
+  {
+    id: 3,
+    title: "Espresso",
+  },
+  {
+    id: 4,
+    title: "Mocha",
+  },
+  {
+    id: 5,
+    title: "Americano",
+  },
+];
+
+export const coffeeSize = [
+  {
+    id: 1,
+    size: "Small",
+  },
+  {
+    id: 2,
+    size: "Medium",
+  },
+  {
+    id: 3,
+    size: "Large",
+  },
+];
+
+export const coffeeItems = [
+  {
+    id: 1,
+    name: "Black Coffee",
+    price: "25.50",
+    volume: "116 ml",
+    stars: "4.6",
+    count:1,
+    image: require("../image/coffee1.png"),
+    desc: "The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.",
+  },
+
+  {
+    id: 2,
+    name: "Cappuccino",
+    price: "15.50",
+    volume: "110 ml",
+    stars: "4.3",
+    count:1,
+    image: require("../image/coffee2.png"),
+    desc: "The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.",
+  },
+
+  {
+    id: 3,
+    name: "Espresso",
+    price: "30.00",
+    volume: "100 ml",
+    stars: "4.0",
+    count:1,
+    image: require("../image/coffee3.png"),
+    desc: "The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.",
+  },
+
+  {
+    id: 4,
+    name: "Latte",
+    price: "10.30",
+    volume: "80 ml",
+    stars: "3.5",
+    count:1,
+    image: require("../image/coffee4.png"),
+    desc: "The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.",
+  },
+
+  {
+    id: 5,
+    name: "Mocha",
+    price: "23.10",
+    volume: "120 ml",
+    stars: "4.7",
+    count:1,
+    image: require("../image/coffee5.png"),
+    desc: "The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.",
+  },
+];
